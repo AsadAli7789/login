d360cc97091d2afd89bde264499bde9d29306852
AsadAli7789 <155202559+AsadAli7789@users.noreply.github.com>
2024-07-08 14:26:55 -0700

    d

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
-  import { getAuth,
+  import { getAuth,signOut,
      createUserWithEmailAndPassword ,signInWithEmailAndPassword 
 
    } from  "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+
+
+   import {
+    getFirestore,
+    collection,
+    addDoc,
+    getDocs,
+  } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js"
+
+
+
 
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -34,6 +45,9 @@
   var email1 = document.getElementById('olp')
   var btn1 = document.getElementById('dcl')
   btn1.addEventListener('click',user1)
+  var btn2 = document.getElementById('dll')
+  btn2.addEventListener('click',logoutuser)
+
 
 
 
@@ -46,7 +60,9 @@
     // Signed up 
     const user = userCredential.user;
     console.log('user sign in')
-
+    hjk.style.display = "block";
+    nml.style.display = "none";
+  
     // ...
   })
   .catch((error) => {
@@ -67,6 +83,10 @@
     console.log(user)
     console.log('u have sign up')
     // ...
+    hjk.style.display = "block";
+    nml.style.display = "none";
+    
+
   })
   .catch((error) => {
     const errorCode = error.code;
@@ -75,3 +95,43 @@
 
 
   }
+  function logoutuser() {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        hjk.style.display = "none";
+        nml.style.display = "block";
+      })
+      .catch((error) => {
+        console.log("logouterror", error);
+      });
+  }
+  const db = getFirestore(app);
+
+
+  var dat1 = document.getElementById('mbl')
+  var credit = document.getElementById('fuc')
+  
+ 
+  var name = document.getElementById('mop')
+  var last_name = document.getElementById('lmp')
+
+
+
+
+
+async function pol(email,password){
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      
+      
+      email: email,
+      password: password,
+    
+    
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
